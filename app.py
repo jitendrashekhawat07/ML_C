16b35e07ef607cd42417b71dc7b993d93111583f
jitendra singh shekhawat <jsshekhawat2005@gmail.com>
2025-07-15 13:32:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st 
+import os 
+import google.generativeai as genai 
+
+genai.configure(api_key= os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.0-flash")
+
+def my_output(query) :
+    response = model.generate_content(query)
+    return response.text
+
+st.set_page_config(page_title="sync_pro_bot")        
+st.header("sync_pro_bot")
+input  = st.text_input("Input" , key ="input")
+submit = st.button("Ask your query")
+
+if submit :
+    response =my_output(input)
+    st.subheader= (" The Response is :")
+    st.write(response)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from heltro!")
+
+
+if __name__ == "__main__":
+    main()
